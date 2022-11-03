f72ff1f53361c649c5b31f16e6d55d93a6875804
Brent S. Rodriguez <brent.s.rodriguez@gmail.com>
2022-11-03 18:36:09 -0500

    Created Widgets

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import './Widget.scss'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
+import ShoppingCartOutlineIcon from '@mui/icons-material/ShoppingCartOutlined'
+import MonetizationOutlineIcon from '@mui/icons-material/MonetizationOnOutlined'
+import AccountBalanceWalletOutlineIcon from '@mui/icons-material/AccountBalanceWalletOutlined'
+
+
+export const Widget = ({ type }) => {
+
+  let data;
+
+  // Temporary Data
+  const amount = 100
+  const diff = 20
+
+  switch (type) {
+    case 'user':
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+        <PersonOutlineIcon 
+        className='icon' 
+        style={{
+          backgroundColor: "rgba(255, 0, 0, 0.2)",
+          color: "crimson",
+        }}
+        />),
+      }
+      break
+    case 'order':
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+        <ShoppingCartOutlineIcon 
+        className='icon' 
+        style={{
+          backgroundColor: "rgba(218, 165, 32, 0.2)",
+          color: "goldenrod",
+        }}
+        />),
+      }
+      break
+    case 'earning':
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+        <MonetizationOutlineIcon 
+        className='icon' 
+        style={{
+          backgroundColor: "rgba(0, 128, 0, 0.2)",
+          color: "green",
+        }}
+        />),
+      }
+      break
+    case 'balance':
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See all balance",
+        icon: (<AccountBalanceWalletOutlineIcon
+         className='icon' 
+         style={{
+          backgroundColor: "rgba(128, 0, 128, 0.2)",
+          color: "purple",
+        }}
+         />),
+      }
+      break
+    default:
+      break;
+  }
+   
+
+
+  return (
+    <div className='widget'>
+      <div className='left'>
+        <span className='title'>{data.title}</span>
+        <span className='counter'>{data.isMoney && '$'} {amount}</span>
+        <span className='link'>{data.link}</span>
+      </div>
+      <div className='right'>
+        <div className='percentage positive'>
+          <KeyboardArrowUpIcon />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
+import { Widget } from '../../components/widget/Widget'
 import "./Home.scss"
 
 const Home = () => {
@@ -10,7 +11,10 @@ const Home = () => {
       <div className='homeContainer'>
         <Navbar />
         <div className='widgets'>
-          
+          <Widget type='user'/>
+          <Widget type='order'/>
+          <Widget type='earning'/>
+          <Widget type='balance'/>
         </div>
       </div>
     </div>
